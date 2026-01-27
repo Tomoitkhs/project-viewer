d75cf7cecba1d9446b3791fc36c6839ce332d62a
Tomoitkhs <tomoitkhs@icloud.com>
2026-01-27 20:25:25 +0000

    git add . git commit -m "Fix: use process.env.PORT for Render" git push

--- index.js
+++ index.js
@@ -276,4 +276,7 @@ io.on("connection", socket => {
   });
 });
 
-server.listen(3000, () => console.log("Server started"));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});
